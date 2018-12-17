9fa2789b1d47124bfb7faaceed08809ef9627088
Jiazheng <jiazheng@wirelessprv-10-193-243-84.near.illinois.edu>
2018-12-16 20:43:11 -0600

    added fastest version by reducing data structure

---
+++ separate_data_structure/step3_driver.py
@@ -0,0 +1,77 @@
+import math
+import numpy as np
+class step3_driver:
+    def __init__(self,target_boxes,neighbor_source_boxes_starts,neighbor_source_boxes_lists,src_weights,tree):
+        self.target_boxes = target_boxes
+        self.neighbor_source_boxes_starts = neighbor_source_boxes_starts
+        self.neighbor_source_boxes_lists = neighbor_source_boxes_lists
+        self.src_weights = src_weights
+        self.wrangler = step3_wrangler(tree.nboxes,tree.ntargets,
+                                       tree.box_source_starts,tree.box_source_counts_nonchild,tree.box_target_starts,
+                                       tree.box_target_counts_nonchild)
+    def multicore_step3(self,total_processors,part):
+        direct_interaction = self.wrangler.eval_direct_multicore(
+            self.target_boxes,
+            self.neighbor_source_boxes_starts,
+            self.neighbor_source_boxes_lists,
+            self.src_weights,total_processors,part)
+
+        # self.recorder.add("eval_direct", self.timing_future)
+        return direct_interaction
+
+
+class step3_wrangler:
+    def __init__(self,nboxes,ntargets,box_source_starts,box_source_counts_nonchild,box_target_starts,box_target_counts_nonchild):
+        self.nboxes = nboxes
+        self.ntargets = ntargets
+        self.box_source_starts = box_source_starts
+        self.box_source_counts_nonchild = box_source_counts_nonchild
+        self.box_target_starts = box_target_starts
+        self.box_target_counts_nonchild = box_target_counts_nonchild
+
+    def _get_source_slice(self, ibox):
+        pstart = self.box_source_starts[ibox]
+        return slice(
+            pstart, pstart + self.box_source_counts_nonchild[ibox])
+
+    def _get_target_slice(self, ibox):
+        pstart = self.box_target_starts[ibox]
+        return slice(
+            pstart, pstart + self.box_target_counts_nonchild[ibox])
+    def output_zeros(self):
+        return np.zeros(self.ntargets, dtype=np.float64)
+    def eval_direct_multicore(self,target_boxes, neighbor_sources_starts,
+            neighbor_sources_lists, src_weights, total_parts, part):
+        from time import time
+        ti = time()
+        pot = self.output_zeros()
+
+        length = math.ceil(self.nboxes / total_parts)
+
+
+        ops = 0
+        seg_start = part * length
+        seg_end = seg_start + length
+
+        if part == total_parts - 1:
+            seg_end = self.nboxes
+        #print("total number of boxes is " + str(self.tree.nboxes) + ",start at :" + str(seg_start) + "    end at: " + str(seg_end))
+        for itgt_box, tgt_ibox in enumerate(target_boxes[seg_start:seg_end]):
+            itgt_box += seg_start
+            tgt_pslice = self._get_target_slice(tgt_ibox)
+
+            src_sum = 0
+            nsrcs = 0
+            start, end = neighbor_sources_starts[itgt_box:itgt_box + 2]
+            # print "DIR: %s <- %s" % (tgt_ibox, neighbor_sources_lists[start:end])
+            for src_ibox in neighbor_sources_lists[start:end]:
+                src_pslice = self._get_source_slice(src_ibox)
+                nsrcs += src_weights[src_pslice].size
+
+                src_sum += np.sum(src_weights[src_pslice])
+
+            pot[tgt_pslice] = src_sum
+            ops += pot[tgt_pslice].size * nsrcs
+        #print("executing direct eval for part"+str(part)+": "+str(time() - ti))
+        #print(pot)
+        return pot  # , self.timing_future(ops)
---
+++ separate_data_structure/step3_splited.py
@@ -0,0 +1,225 @@
+from typing import Type
+
+from step3_driver import step3_driver
+from charm4py import charm, Chare, Group, Reducer,Array,threaded,ArrayMap,Options
+from charm4py import readonlies as ro
+import sys
+
+from my_own_test import Mytest
+import numpy as np
+from boxtree import *
+from my_own_fmm import *
+from time import time
+
+class MyChare(Chare):
+    def __init__(self,future,total):
+
+
+        self.flag = None
+        self.time = None
+        self.future = future
+        self.driver = None
+        self.total = total
+    def summation_setter(self,driver):
+        self.driver = driver
+
+    def flag_setter(self,flag):
+        self.flag = flag
+    def time_setter(self,time):
+        self.time = time
+
+    #@threaded
+    def work(self):
+
+        if self.flag == "4":
+            start = time()
+            print("step4 on "+str( charm.myPe()))
+            #self.driver.step4()
+            #self.driver.step6()
+            self.contribute(self.driver.separate_step4(), Reducer.sum, self.future)
+            print("step4: " + str(time() - start))
+
+        elif self.flag == "6":
+        #print("adjnaskaajsdnkjsanksaksnkajnk")
+            start = time()
+            print("step6 on "+str(charm.myPe()))
+            self.contribute(self.driver.separate_step6(), Reducer.sum, self.future)
+            print("step6: " + str(time() - start))
+        else:
+            self.contribute(np.zeros(self.total), Reducer.sum, self.future)
+
+    def collectResult(self, result):
+
+        result = self.driver.wrangler.reorder_potentials(result)
+        end = time()
+        print( end- self.time)
+
+
+
+        exit()
+
+def hello(args):
+    my_group = Group(MyChare)
+    my_group.work(3)
+
+class Distributed_Driver(Chare):
+    def __init__(self):
+
+        pass
+
+    def work(self, driver,flag):
+        data = 3
+        print(data)
+        self.contribute(data,Reducer.sum,self.sumation_chare.collectResult)
+
+
+
+class WorkerMap(ArrayMap):
+    def procNum(self, index):
+        #print(index)
+        return (index[0] % (charm.numPes() - 1)) + 2
+class ExpWorkerMap(ArrayMap):
+    def procNum(self, index):
+        #print(index)
+        return (index[0] % (charm.numPes() - 1)) + 1
+
+class step3_chare(Chare):
+    def __init__(self,total_processors,f,driver):
+
+
+        self.total_processors = total_processors
+        self.f = f
+        self.pos = 0
+        self.driver = driver
+    def index_setter(self,index):
+        self.pos = index
+    def calculate(self):
+        print("this piece "+str(self.pos) + "   is on processor:" + str(charm.myPe()))
+        st = time()
+        partial_direct_interaction = self.driver.multicore_step3(self.total_processors,self.pos)
+        self.contribute(partial_direct_interaction,Reducer.sum,self.f)
+        print("time to calculate "+str(self.pos)+ "th piece of the direct interaction:" +str(time() - st))
+
+
+
+def main(args):
+
+    my = Mytest()
+    tree = my.tree
+    driver = my.cal()
+    ti = time()
+    very_start = time()
+
+    f = charm.createFuture()
+    f_other = charm.createFuture()
+    creation_time = time()
+
+    step3_dr = step3_driver(driver.traversal.target_boxes,driver.traversal.neighbor_source_boxes_starts,
+                            driver.traversal.neighbor_source_boxes_lists,
+                            driver.src_weights,tree)
+    step3_array = Array(step3_chare, args=[6, f,step3_dr], dims=6,map=Group(WorkerMap))
+    my_array = Array(MyChare, args=[f_other,tree.nsources], dims=2)
+    print("hehhehhehe")
+    #a = np.zeros(9000000)
+    #driver.step3()
+
+    for i in range(0,6):
+        step3_array[i].index_setter(i)
+    step3_array.calculate()
+    driver.step21()
+    driver.step22()
+
+
+    #print("creation for step3 finished " + str(time() - creation_time))
+
+    #start = time()
+    #print(start - ti)
+    #my_array = Array(MyChare, args=[driver, f_other], dims=2)
+
+    # create one instance of MyChare on every processor
+    #my_group = Group(MyChare)
+
+    # create 3 instances of MyChare, distributed among the cores by the runtime
+
+    #first = MyChare()
+
+    # create 2 x 2 instances of MyChare, indexed using 2D index and distributed
+    # among all cores by the runtime
+
+    #my_2d_array = Array(MyChare, (2, 2))
+    #charm.awaitCreation(my_array)
+
+    #while(True):
+    #    pass
+    #print("###############################3")
+    #charm.awaitCreation(first)
+
+
+    #charm.awaitCreation(my_array)
+
+    #from CustomGreen import CustomConstantOneExpansionWrangler
+    #c = CustomConstantOneExpansionWrangler(tree)
+    #my_array[0].time_setter(ti)
+    #my_array[0].summation_setter(driver)
+
+    my_array[0].summation_setter(driver)
+    my_array[0].flag_setter("4")
+
+    my_array[1].summation_setter(driver)
+    my_array[1].flag_setter("6")
+    #my_array[1].driver = driver
+    my_array.work()
+
+    tii = time()
+    #print("time for creating two array, step1,2 and mmy array start:" + str(time() - start))
+    #my_array.work()
+    local_result = driver.step5()
+    if driver.traversal.from_sep_close_bigger_starts is not None:
+        step_6_extra = driver.step6_extra()
+        print("extra step6 time: "+str(time() - tii))
+        local_result += step_6_extra
+    local_result += f.get()
+    #print(local_result)
+    print("time to get local_result:" + str(time() - tii))
+
+
+    local_exps = f_other.get()
+
+    local_exps = driver.wrangler.refine_locals(driver.traversal.level_start_target_or_target_parent_box_nrs,
+                                  driver.traversal.target_or_target_parent_boxes,
+                                  local_exps)
+    local_result_from_exp = driver.wrangler.eval_locals(
+        driver.traversal.level_start_target_box_nrs,
+        driver.traversal.target_boxes,
+        local_exps)
+
+
+    #
+    end = time()
+    result = driver.wrangler.reorder_potentials(local_result_from_exp + local_result)
+    print("at the end:"+str(end - very_start))
+    #print(result)
+    assert (result == tree.nsources).all()
+    #charm.printStats()
+    exit()
+
+
+    #assert (result == 9000000).all()
+
+
+
+    #future = my_array.work(li, ret=True)
+    #individual = MyChare
+
+    #individual
+
+
+
+
+if __name__ == "__main__":
+    #
+    #Options.PROFILING = True
+    ti = time()
+    charm.start(main)  # call main([]) in interactive mode
+    #main("a")
+    #print(time() - ti)
